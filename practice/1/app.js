016f811e014797ef21372d9be830b437c71c50bc
Burakova Valentina <v.m.30.03.20@gmail.com>
2024-08-02 15:38:09 +0300

    feat: static

--- practice/1/app.js
+++ practice/1/app.js
@@ -1,6 +1,6 @@
-const modal = document.querySelector(".modal");
-const button = document.querySelector(".start");
 function handleOpenModal() {
+  const modal = document.querySelector(".modal");
+  const button = document.querySelector(".modal__open-button");
   modal.classList.toggle("open");
   button.classList.toggle("hid");
 }
@@ -10,21 +10,51 @@ document
   .addEventListener("change", function (event) {
     const files = event.target.files;
     const preview = document.getElementById("preview");
-
-    // Clear any existing content
+    const fileLabel = document.getElementById("fileLabel");
 
     file = files[0];
-    // Loop through all selected files
     const imgContainer = document.createElement("div");
     const img = document.createElement("img");
     img.src = URL.createObjectURL(file);
     img.style.height = "100%";
     img.style.width = "100%";
-    img.style.display = "block"; // Ensure the image is displayed in a block to put it on a new line
-    // Append the image and file info to the container
+    img.style.display = "block";
     imgContainer.appendChild(img);
 
-    // Append the container to the preview div
     preview.innerHTML = "";
+    fileLabel.classList.toggle("imglogo__input-img");
     preview.appendChild(imgContainer);
   });
+
+function handleDeleteIcon() {
+  const preview = document.getElementById("preview");
+  const fileLabel = document.getElementById("fileLabel");
+  preview.innerHTML = "";
+  const imgContainer = document.createElement("div");
+  const img = document.createElement("img");
+  img.src = "../assets/images/logo.png";
+  img.style.height = "100%";
+  img.style.width = "100%";
+  img.style.display = "block";
+  imgContainer.appendChild(img);
+  preview.appendChild(imgContainer);
+  fileLabel.classList.toggle("imgCross");
+}
+
+document.getElementById("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  const modal__inputs = document.querySelectorAll("#form input");
+  let isValid = true;
+
+  modal__inputs.forEach((input) => {
+    if (!input.checkValidity()) {
+      isValid = false;
+    }
+  });
+
+  if (isValid) {
+    alert("Form submitted successfully!");
+  } else {
+    alert("Please fill out all required fields.");
+  }
+});
