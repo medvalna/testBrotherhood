3f058c24b3a812e6df9a30bf73c735a9727507f6
Burakova Valentina <v.m.30.03.20@gmail.com>
2024-08-11 13:35:07 +0200

    fix: mask for phone input

--- practice/1/app.js
+++ practice/1/app.js
@@ -67,5 +67,5 @@ document.getElementById("input__phone").addEventListener("input", function (e) {
     .replace(/\D/g, "")
     .match(/(\d{0,1})(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/);
   e.target.value =
-    "+" + x[1] + "- (" + x[2] + ")" + "-" + x[3] + "-" + x[4] + "-" + x[5];
+    "+" + x[1] + " " + x[2] + " " + x[3] + "-" + x[4] + "-" + x[5];
 });
